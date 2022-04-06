311d318b1efa6d2c42cc72a792134401008bc1d7
Joemon V Velassery <joemonv97@gmail.com>
2022-04-06 16:35:18 +0530

    UI Shape

--- management/urls.py
+++ management/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path("",views.index),
+    path("",views.index,name="h"),
     path("login",views.LogInUser.as_view(),name="Login")
 ]
